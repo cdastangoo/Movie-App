870accaf43c5cf36a60147e1e2227930419397b8
Cyrus <cydastang@gmail.com>
2021-11-18 00:51:01 -1000

    added hero banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,23 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import NoImage from '../images/no_image.jpg';
 
+import HeroImage from './HeroImage';
+
 const Home = () => {
   const { movies, loading, error, errorMessage } = useHomeFetch();
   console.log(movies);
-  return <div>Home Page</div>;
+  return (
+    <>
+      {movies.results[0] ?
+        <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movies.results[0].backdrop_path}`}
+            title={movies.results[0].original_title}
+            text={movies.results[0].overview}
+        />
+      : null
+      }
+    </>
+  );
 };
 
 export default Home;
